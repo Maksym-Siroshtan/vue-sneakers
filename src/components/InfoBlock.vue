ecfc2d9a2cc49aa7e39195598940497fe11e21b6
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-01-20 20:04:02 +0200

    Added InfoBlock component

---
+++ src/components/InfoBlock.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: String,
+  description: String,
+  imageUrl: String
+})
+</script>
+
+<template>
+  <div class="flex flex-col items-center w-70">
+    <img :src="imageUrl" alt="Info-image" class="w-36" />
+    <h2 class="text-2xl font-bold mt-4">{{ title }}</h2>
+    <p class="text-xl text-slate-400 text-center mt-2">{{ description }}</p>
+  </div>
+</template>
